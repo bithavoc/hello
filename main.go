261d3c71297b7482a0086079f4e1992cdd8fc533
bithavoc <im@bithavoc.io>
2016-03-06 10:31:08 -0500

    test

--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, this is a %s", os.Getenv("STUFF"))
+	fmt.Fprintf(w, "Hi there, this is a %s\n", os.Getenv("STUFF"))
+	fmt.Fprintf(w, "Server")
 	fmt.Println("NIce Shit")
+	log.Println("error test")
 }
 
 func main() {
